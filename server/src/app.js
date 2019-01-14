6df9ee66535c470db7dfd9174a318e7660046416
Raheem McDonald <raheemmcdonald@Raheems-MacBook-Pro.local>
2019-01-14 14:02:48 -0500

    Server setup

--- server/src/app.js
+++ server/src/app.js
@@ -1,2 +1,13 @@
-
 console.log('Hello');
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+app.use(morgan('combine'))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.listen(process.env.PORT)
